c7790572d7ca8e632217d7940b4a9a8917715d46
vkboi5 <vkirthikjan5@gmail.com>
2025-04-26 01:22:12 +0530

    done index

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+export { default as moonpayService } from './moonpayService';
+export { default as corsProxyService } from './corsProxyService';
+export { default as pinataService } from './pinataService'; 
